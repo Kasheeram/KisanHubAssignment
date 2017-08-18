ac56f8505d9d3b895d7e30dbabd4b58bcfd753d7
Apogee <apogee@Apogees-MacBook-Pro.local>
2017-08-18 17:15:11 +0530

    able to fetch data from url

--- KisanHubAssignment/ViewController.swift
+++ KisanHubAssignment/ViewController.swift
@@ -12,7 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let requestURL: NSURL = NSURL(string:  "http://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Tmean/ranked/UK.txt")!
+        let url = URL(string: "http://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Tmean/ranked/UK.txt")
+        
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+           // print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue))
+            let datavalue = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            let myStrings = datavalue?.components(separatedBy: .newlines)
+            //let string = myStrings?.joined(separator: ", ")
+            print(myStrings!.count)
+            let str = myStrings![115]
+            print(str)
+            let components = str.components(separatedBy: .whitespacesAndNewlines)
+            let words = components.filter { !$0.isEmpty }
+            //print(words[0])
+        }
+        
+        task.resume()
+
     }
 
     override func didReceiveMemoryWarning() {
